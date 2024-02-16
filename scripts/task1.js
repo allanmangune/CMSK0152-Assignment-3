4e2f63c080f0f4ec46c69666fb61ed488b111d34
allan <allanmangune@hotmail.com>
2024-02-15 17:56:59 -0700

    Completed source files.

---
+++ scripts/task1.js
@@ -0,0 +1,4 @@
+let countries = ["Canada", "United States of America", "India", "France", "Mexico", "Philippines"];
+countries.forEach(country=> {
+    console.log(country);
+});
---
+++ scripts/toothbrush-data.js
@@ -0,0 +1,9 @@
+class ToothbrushData{
+    constructor(){
+        this.data = [];
+    }
+
+    addToothbrush(toothbrush){
+        this.data.push(toothbrush);
+    }
+}
