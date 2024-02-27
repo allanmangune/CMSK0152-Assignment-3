31c007b9db4ff9c57fdaa4527861754b18bb8f4b
allan <allanmangune@hotmail.com>
2024-02-27 16:50:24 -0700

    Updated the file.

--- scripts/task1.js
+++ scripts/task1.js
@@ -1,4 +1,16 @@
 let countries = ["Canada", "United States of America", "India", "France", "Mexico", "Philippines"];
-countries.forEach(country=> {
+countries.forEach((country)=> {
     console.log(country);
 });
+
+/* alternatively
+let countries = [];
+countries[0] = "Canada";
+countries[1] = "United States of America";
+countries[2] = "India";
+countries[3] = "France";
+countries[4] = "Mexico";
+countries[5] = "Philippines";
+countries.forEach((country)=> {
+    console.log(country);
+*/
